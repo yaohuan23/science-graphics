868f7a8ac5285fab3587191f4fd330faa6690afd
lafita <aleixlafita@gmail.com>
2016-01-21 19:08:05 +0100

    Nicer PR curve

--- source/Benchmarking.R
+++ source/Benchmarking.R
@@ -2,7 +2,7 @@
 # Aleix Lafita - 01.2016
 
 library(ggplot2)
-library(reshape)
+library(reshape2)
 library(plyr)
 
 #' Represents the accuracy of the classifier for each label, coloring
@@ -14,11 +14,11 @@ multiLabelPlot <- function(data) {
   
   # Scale the data to 100%
   mtable = melt(table)
-  scaled <- ddply(mtable, "Truth", transform,
+  scaled = ddply(mtable, "Truth", transform,
                  Percentage = value / sum(value) * 100)
   
   # Plot the scaled data
-  p <- ggplot(scaled, aes(x=Truth, y=Percentage, fill=Prediction,
+  p = ggplot(scaled, aes(x=Truth, y=Percentage, fill=Prediction,
                           alpha=as.character(Truth)==as.character(Prediction))) + 
     geom_bar(stat="identity") + 
     scale_alpha_discrete(range=c(0.5,1)) +
@@ -32,13 +32,13 @@ multiLabelPlot <- function(data) {
 toClassificationTable <- function(data) {
   
   # Ensure propper names of the data and all factors
-  names(data) <- c("Truth", "Prediction")
-  data$Truth <- factor(data$Truth)
-  data$Prediction <- factor(data$Prediction)
+  names(data) = c("Truth", "Prediction")
+  data$Truth = factor(data$Truth)
+  data$Prediction = factor(data$Prediction)
   
   # Transform the raw data into a classification table
-  data$Count <- rep(1,nrow(data)) # add new column of ones
-  table <- cast(data, Truth ~ Prediction, sum, value="Count")
+  data$Count = rep(1,nrow(data)) # add new column of ones
+  table = cast(data, Truth ~ Prediction, sum, value="Count")
 }
 
 #' Represent the precision of a ranking result in function of the number
@@ -48,35 +48,21 @@ toClassificationTable <- function(data) {
 precisionRecallCurve <- function(data) {
   
   # Ensure propper names of the data
-  names(data) <- c("Relevance")
+  names(data) = c("Relevance")
   
   # Transform the data into accumulative precision values
-  data$Count <- rep(1,nrow(data)) # add new column of ones
+  data$Count = rep(1,nrow(data)) # add new column of ones
   data$RelSum = cumsum(data$Relevance) # calculate the relevance sum
-  data$Recall <- cumsum(data$Count)
-  data$Precision <- data$RelSum / data$Recall
+  data$Recall = cumsum(data$Count)
+  data$P1 = data$RelSum / data$Recall
+  data$P2 = rev(cummax(rev(data$P1)))
+  
+  # Join precision and nice precision for plotting
+  table = melt(data[,c("Recall","P1","P2")], id.vars = "Recall", value.name = "Precision")
   
   # Create the curve
-  p <- ggplot(data, aes(x=Recall, y=Precision)) + geom_line()
-}
-
-#' Represent the precision of a ranking result in function of the number
-#' of retrieved results (recall) in a nicer way: starting from the back,
-#' use only the cumulative maximum of the precision.
-#' @param data a column with a 1 if the result is relevant, 0 otherwise
-#' @return a ggplot object
-nicePrecisionRecallCurve <- function(data) {
-  
-  # Ensure propper names of the data
-  names(data) <- c("Relevance")
-  
-  # Transform the data into accumulative precision values
-  data$Count <- rep(1,nrow(data)) # add new column of ones
-  data$RelSum = cumsum(data$Relevance) # calculate the relevance sum
-  data$Recall <- cumsum(data$Count)
-  data$Precision <- data$RelSum / data$Recall
-  data$NicePrecision <- rev(cummax(rev(data$Precision)))
-  
-  # Create the curve
-  p <- ggplot(data, aes(x=Recall, y=NicePrecision)) + geom_line()
+  p = ggplot(table, aes(x=Recall, y=Precision, colour=variable)) + 
+    geom_line() +
+    guides(colour=FALSE) +
+    theme_bw()
 }

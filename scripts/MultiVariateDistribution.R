edaa13a4af60eae57977b885dc2c0d021e3d9ea4
lafita <aleixlafita@gmail.com>
2016-02-18 17:54:24 +0100

    Create multivariate distribution plot

---
+++ scripts/MultiVariateDistribution.R
@@ -0,0 +1,49 @@
+#!/usr/bin/env Rscript
+# This script plots the distribution of multiple variables of the same units.
+# The density and underlying histogram of each variable are shown.
+# The format of the input is in two columns, variable name and value.
+# An additional name column can be optionally given in the first column,
+# but it will be ignored.
+# Aleix Lafita - 02.2016
+
+# Import all the source files needed
+source("../source/InputOutput.R")
+source("../source/Distribution.R")
+
+# Default input parameters
+project = "example1"
+binSize = 0
+minX = 0
+maxX = 0
+
+cat("##### MULTIVARIATE DISTRIBUTION PLOT #####\n")
+
+# Parse args if executed from the cmd line
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  cat(paste("No argument given.\n"))
+} else if (length(args)==1) {
+  project = args[1]
+} else if (length(args)==2) {
+  project = args[1]
+  binSize = as.double(args[2])
+} else if (length(args)==3) {
+  project = args[1]
+  binSize = as.double(args[2])
+  minX = as.double(args[3])
+  maxX = minX
+} else if (length(args)==4) {
+  project = args[1]
+  binSize = as.double(args[2])
+  minX = as.double(args[3])
+  maxX = as.double(args[4])
+}
+cat(paste("Using arguments:", project, binSize, minX, maxX, "\n"))
+
+data = parseFile(project)
+if (ncol(data) > 2){
+  data = data[c(2,3)]
+}
+
+p = plotHistogramDensity(data, binSize, minX, maxX)
+saveFigure(project, p)
---
+++ source/Distribution.R
@@ -0,0 +1,47 @@
+# Plot variable distributions
+# Aleix Lafita - 02.2016
+
+library(ggplot2)
+library(plyr)
+
+#' Plot the data distribution of the variables as a density with 
+#' underlying histogram.
+#' The histogram is scaled to the density scale.
+#' 
+#' @param data two columns, variable name and value
+#' @param bin bin size to use, 0 is default to min(range/(points/(5*variables)),30)
+#' @param xmin minimum value of the variable range
+#' @param xmax maximum value of the variable range
+#' @return ggplot2 object
+plotHistogramDensity = function(data, bin=0, xmin=0, xmax=0) {
+  
+  # Default bin size calculation
+  if (bin == 0){
+    type = data
+    names(type) = c("Type", "Value")
+    levels = nlevels(type$Type)
+    bins = as.integer(nrow(data)/(1*levels))
+    range = xmax - xmin
+    if (range == 0) {
+      maxmin = range(data[2])
+      range = maxmin[2]-maxmin[1]
+      range = range + 0.01 * range
+    }
+    bin = range / min(bins, 30)
+  }
+  
+  p = ggplot(data, aes_q(x=as.name(names(data)[2]),
+                         y=quote(..density..),
+                         fill=as.name(names(data)[1]),
+                         color = as.name(names(data)[1]))) +
+    geom_histogram(position="identity", alpha = 0.3, colour=NA, 
+                   binwidth = bin) + 
+    geom_line(stat="density") +
+    theme_bw()
+  
+  if (xmin < xmax) {
+    p = p + xlim(xmin, xmax)
+  }                  
+  return(p)
+
+}

fca93cec85ddc6b20788286d589eadbdfe0ec371
lafita <aleixlafita@gmail.com>
2016-04-19 12:24:55 +0200

    Replace helper scripts by script option #24

--- helper/TsvToCsv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $1 ]; then
-    echo "Converting data/${1}.tsv from TSV to CSV"
-    sed 's/\t/\,/g' ../data/${1}.tsv > ../data/${1}.csv
-    echo "Writing ${1}.csv to data folder"
-    
-else
-    echo ERROR: Need to provide project name
-fi

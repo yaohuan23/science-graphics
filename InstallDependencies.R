94122a5cffbc0aa82fe2dee552efda12d339635b
lafita <aleixlafita@gmail.com>
2016-04-21 16:35:02 +0200

    Nicer example figures for discrete distributions

--- InstallDependencies.R
+++ InstallDependencies.R
@@ -2,12 +2,10 @@
 # This script installs all the required dependencies for science-graphics
 # R version 3.0.2 (2013-09-25) or higher
 
-source("source/ScienceGraphicsIO.R")
-printSGheader("Install Dependencies")
-
 rep='http://cran.us.r-project.org'
 #lib=path/to/lib/ if any preference
 
+install.packages("optparse", repos=rep)
 install.packages("ggplot2", repos=rep)
 install.packages("gridExtra", repos=rep)
 install.packages("grid", repos=rep)
@@ -16,3 +14,5 @@ install.packages("plyr", repos=rep)
 install.packages("mlearning", repos=rep)
 install.packages("igraph", repos=rep)
 install.packages("ROCR", repos=rep)
+
+cat("   Installation Done!")
